c30bf9c07bd13559085bde4645d6e5c764d29f9f
Akshara-sandha <sandhaakshara@gmail.com>
2024-06-28 14:12:09 +0530

    Update Checker.cpp
    code changes

--- Checker.cpp
+++ Checker.cpp
@@ -6,7 +6,7 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
   if(temperature < 0 || temperature > 45) {
     cout << "Temperature out of range!\n";
     return false;
-  } else if(soc < 20 || soc > 80) {
+  } else(soc < 20 || soc > 80) {
     cout << "State of Charge out of range!\n";
     return false;
   } else(chargeRate > 0.8) {
